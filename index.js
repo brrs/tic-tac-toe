04697d285cafb07ad2bd09310e9110de976cd479
brrs <re.proxy0@gmail.com>
2022-09-03 08:41:29 +0300

    now it's better

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const states = { X: 'x' ,  O: 'o', EMPTY: ""}
+const states = { X: 'X' ,  O: 'O', EMPTY: ""}
 
 function createTicTacToe(onWin, onDraw)  {
     return {
@@ -9,61 +9,80 @@ function createTicTacToe(onWin, onDraw)  {
         ],
         turn: states.X,
         turnCount: 0,
-        makeTurn: function(x, y, updateView) {
+        winner: "",
+        makeTurn: function(x, y, updateView, onWrongCell) {
+            // check if the game is finished and coords is ok
+            if (this.winner !== "") return
             if (x > 3 || x < 0 || y > 3 || y < 0)  {
                 console.log("Wrong coordinates")
                 return
-            }
+            }   
+
+            // check if the cell is already taken
             if (this.grid[x][y] == states.EMPTY) {
+                // make turn
                 this.grid[x][y] = this.turn
                 updateView(this.turn)
 
-                // check 
-                for (let i = 0; i < 3; i++) {
-                    if (this.grid[x][i] != this.turn) break
-                    if (i == 2) {
-                        onWin(this.turn)
-                    }
-                }
-
-                for(let i = 0; i < 3; i++){
-                    if(this.grid[i][y] != this.turn) break
-                    if (i == 2) {
-                        onWin(this.turn)
-                    }
-                }
-
-                if (x == y) {
-                    for (let i = 0; i < 3; i++) {
-                        if(this.grid[i][i] != this.turn) break
-                        if(i == 2){
-                            onWin(this.turn)
-                        }
-                    }
-                }
-
-                if(x + y == 2){
-                    for (let i = 0; i < 3; i++) {
-                        if(this.grid[i][2-i] != this.turn) break
-                        if(i == 2){
-                            onWin(this.turn)
-                        }
+                // check win or draw
+                if (check(x, y, this.turn, this.grid)) {
+                    this.winner = this.turn
+                    onWin(this.turn)
+                    return
+                } else {
+                    if (this.turnCount == 8) {
+                        onDraw()
+                        return
                     }
                 }
                 
-                if (this.turnCount == 8) onDraw()
+                // prepare for next turn
                 if (this.turn == states.X) this.turn = states.O; else this.turn = states.X
                 this.turnCount++
             } else {
-                console.log("This cell is already taken")
+                onWrongCell()
             }
         }
     }
 }
 
-const caption = document.getElementById("caption")
-caption.textContent = ""
+function check(x, y, turn, state) {
+    for (let i = 0; i < 3; i++) {
+        if (state[x][i] != turn) break
+        if (i == 2) {
+            return true
+        }
+    }
 
+    for(let i = 0; i < 3; i++){
+        if(state[i][y] != turn) break
+        if (i == 2) {
+            return true
+        }
+    }
+
+    if (x == y) {
+        for (let i = 0; i < 3; i++) {
+            if(state[i][i] != turn) break
+            if(i == 2){
+                return true
+            }
+        }
+    }
+
+    if(x + y == 2){
+        for (let i = 0; i < 3; i++) {
+            if(state[i][2-i] != turn) break
+            if(i == 2){
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+const caption = document.getElementById("caption")
 let ttt = createTicTacToe(
     function(state) {
         caption.textContent = state + " won"
@@ -73,28 +92,26 @@ let ttt = createTicTacToe(
     }
 )
 
+// setting listeners
 const board = document.getElementById("board").children
 for (let i = 0; i < board.length; i++) {    
-    board[i].addEventListener("click", function() {
-        let coords;
-        switch (i) {
-            case 0: coords = {x: 0, y: 0}; break
-            case 1: coords = {x: 1, y: 0}; break
-            case 2: coords = {x: 2, y: 0}; break
-            case 3: coords = {x: 0, y: 1}; break
-            case 4: coords = {x: 1, y: 1}; break
-            case 5: coords = {x: 2, y: 1}; break
-            case 6: coords = {x: 0, y: 2}; break
-            case 7: coords = {x: 1, y: 2}; break
-            case 8: coords = {x: 2, y: 2}; break
-        }
-        console.log(coords.x + " " + coords.y)
-
-        ttt.makeTurn(coords.x, coords.y, function(state){ 
-            console.log(state)
-            let element = document.createElement("p1")
-            element.textContent = state
-            board[i].appendChild(element)
+    const item = document.getElementById(board[i].id)
+    const x  = item.id[0]
+    const y  = item.id[1]
+    item.addEventListener("click", function() {
+        ttt.makeTurn(x, y, function(turn) {
+            console.log("update")
+            let element = document.createElement('img');
+            let img;
+            if (turn == states.X) img = "x.png"; else img = "o.png" 
+            element.src = img
+            element.classList.add("pin")
+            item.appendChild(element)
+        }, function(){
+            item.classList.add("err")
+            setTimeout(function(){
+                item.classList.remove("err")
+                }, 500)
         })
     })
 }
