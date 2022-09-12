7730593b6dc941a2a73216d0e5140e6b3020962b
brrs <re.proxy0@gmail.com>
2022-09-13 00:18:56 +0300

    all in the game class

--- game.js
+++ game.js
@@ -1,15 +1,33 @@
 const states = { X: 'X', O: 'O', EMPTY: "" }
 
 class Game {
-    constructor(_onBoardChange, _onWinOrDraw, _onWrongCell) {
+    constructor(parent) {
         this.turn = states.X
         this.turnCount = 0
         this.winner = states.EMPTY
         this.board = this.getEmptyBoard()
-        // callbacks
-        this.onBoardChange = _onBoardChange
-        this.onWinOrDraw = _onWinOrDraw
-        this.onWrongCell = _onWrongCell
+        this.initHtml(parent)
+    }
+
+    initHtml(parent) {
+        const grid = document.createElement('div')
+        grid.classList.add('grid-container')
+        grid.id = 'board'
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement('div')
+            cell.classList.add('grid-item')
+            cell.id = i
+            cell.addEventListener('click', () => { this.makeTurn(i) })
+            grid.appendChild(cell)
+        }
+        parent.appendChild(grid)
+        this.grid = grid
+
+        const caption = document.createElement('h1')
+        caption.classList.add('caption')
+        caption.id = 'caption'
+        parent.appendChild(caption)
+        this.caption = caption
     }
 
     getEmptyBoard() {
@@ -21,13 +39,15 @@ class Game {
         this.turnCount = 0
         this.winner = states.EMPTY
         this.board = this.getEmptyBoard()
+        this.caption.textContent = ''
+        for (const cell of this.grid.children) { cell.innerHTML = '' }
     }
 
     makeTurn(cell) {
         // check if the game is finished and coords is ok
         if (this.winner !== states.EMPTY) return
         if (cell < 0 || cell > 8) return
-        
+
         // check if the cell is not yet taken
         if (this.isTaken(cell)) {
             this.onWrongCell(cell)
@@ -41,11 +61,11 @@ class Game {
         // check win or draw
         if (this.checkWinner()) {
             this.winner = this.turn
-            this.onWinOrDraw(this.turn)
+            this.onWinOrDraw()
             return
         } else {
             if (this.turnCount == 8) {
-                this.onWinOrDraw(states.EMPTY)
+                this.onWinOrDraw()
                 return
             }
         }
@@ -57,6 +77,27 @@ class Game {
 
     isTaken(cell) {
         return this.board[cell] != states.EMPTY
+    }
+
+    onWrongCell(cell) {
+        this.grid.children[cell].classList.add("err").setTimeout(() => { element.classList.remove("err") }, 500)
+    }
+
+    onBoardChange(cell) {
+        let child = document.createElement('img');
+        let img;
+        if (game.turn == states.X) img = "x.png"; else img = "o.png"
+        child.src = img
+        child.classList.add("pin")
+        this.grid.children[cell].appendChild(child)
+    }
+
+    onWinOrDraw() {
+        if (this.winner == states.EMPTY) {
+            this.caption.textContent = "draw"
+        } else {
+            this.caption.textContent = this.winner + " won"
+        }
     }
 
     checkWinner() {
